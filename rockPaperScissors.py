31e9adedb358caae84cae023c19df6ad5bd04ee4
Roman <romanka@edu.hac.ac.il>
2022-06-13 12:28:26 +0300

    update file

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -10,45 +10,68 @@ As you can see, this line is also a comment.
 # ----------------------------------------------------------------------------------------------------------------------
 # Here you include all of your package imports (like randome and time packages wev'e discussed about) 
 # ----------------------------------------------------------------------------------------------------------------------
-import random
+#import random
 
 # ----------------------------------------------------------------------------------------------------------------------
-# Here you will write all of the functions (for later stages of the course
+# Here you will write all of the functions (for later stages of the course :
 # ----------------------------------------------------------------------------------------------------------------------
 
 # ----------------------------------------------------------------------------------------------------------------------
 # Here you write code :)
 # ----------------------------------------------------------------------------------------------------------------------
-"""
-I'll give you the text inputs for this program, to make your lives a little easier.
-In addition, and to make it simple to you, the input from the user will be an integer:
-1 for ROCK
-2 for PAPER
-3 for SCISSORS
-A text input describing the operation is unacceptable and will cost you with points.
-
-A quick reminder of the rules:
-
-ROCK(1) vs SCISSORS(3)   --> ROCK(1) wins
-SCISSORS(3) vs PAPER(2)  --> SCISSORS(3) win
-PAPER(2) vs ROCK(1)      --> PAPER(2) wins
-
-
-DO NOT ADD EXTRA OPTIONS (No lizard, no Spock.)
-"""
-
-# print the instructions for the user to see:
-print("GAME RULES: \n"
-      "ROCK(1) vs SCISSORS(3)   --> ROCK(1) wins\n"
-      "SCISSORS(3) vs PAPER(2)  --> SCISSORS(3) win\n"
-      "PAPER(2) vs ROCK(1)      --> PAPER(2) wins)\n")
-
+# """
+# # I'll give you the text inputs for this program, to make your lives a little easier.
+# # In addition, and to make it simple to you, the input from the user will be an integer:
+# # 1 for ROCK
+# # 2 for PAPER
+# # 3 for SCISSORS
+# # A text input describing the operation is unacceptable and will cost you with points.
+# #
+# # A quick reminder of the rules:
+# #
+# # ROCK(1) vs SCISSORS(3)   --> ROCK(1) wins
+# # SCISSORS(3) vs PAPER(2)  --> SCISSORS(3) win
+# # PAPER(2) vs ROCK(1)      --> PAPER(2) wins
+# #
+# #
+# # DO NOT ADD EXTRA OPTIONS (No lizard, no Spock.)
+# # # """
+#
+# # print the instructions for the user to see:
+# print("GAME RULES: \n"
+#       "ROCK(1) vs SCISSORS(3)   --> ROCK(1) wins\n"
+#       "SCISSORS(3) vs PAPER(2)  --> SCISSORS(3) win\n"
+#       "PAPER(2) vs ROCK(1)      --> PAPER(2) wins)\n")
+#
 # The game will run in a WHILE loop.
 # The loop is infinite, and only the user has the power to stop it (when they say they don't want to play anymore)
-while True:
-    """
-      This is the game's heart. You'll need to think and use everything we've learned so far to make this game work.
-      Remember Python's rules ( the ':' after a statement, the indentation with loops and statements..)
-      
-      """
-    pass
+# while True:
+#    """
+#      This is the game's heart. You'll need to think and use everything we've learned so far to make this game work.
+#      Remember Python's rules ( the ':' after a statement, the indentation with loops and statements..)
+#
+#      """
+#    pass
+#"
+print("Game Rules: /n"
+        "Rock(1) vs Scissors(3)   --> Rock(1) wins/n"
+        "Scissors(3) vs Paper(2)  --> Scissors(3) win/n"
+        "Paper(2) vs Rock(1)      --> Paper(2) wins)/n")
+
+print("#Hello User please enter your name:")
+
+User_name = input("Enter User`s name:")
+
+print("Hello" , User_name + " Welcome to Rock Paper Scissor game in python Created by Roman Karpov , Enjoy and have a good time!")
+
+print(User_name + " Please enter your choice : Rock , Paper or Scissors:")
+
+Users_choice = input("Enter" , User_name + "`s choice:")
+
+
+
+gamelist = ("Rock","Paper","Scissors")
+
+
+
+
